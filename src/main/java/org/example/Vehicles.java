9107f4e934a960e3a29804166ec0ca7c8d4af89d
IvanZhyvago <ivan.zhyvago@ctdev.io>
2021-02-10 17:17:56 +0200

    Add new Car

---
+++ src/main/java/org/example/Vehicles.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Vehicles {
+
+    public void car () {
+        System.out.println("HONDA");
+    }
+
+    public static void main(String[] args) {
+        Vehicles vehicles = new Vehicles();
+        vehicles.car();
+    }
+}
